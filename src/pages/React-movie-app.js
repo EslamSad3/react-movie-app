da27b9403c41e41bcaf211b2105753de8c747520
EslamSad3 <eslam.sa3d96@gmail.com>
2023-06-12 01:28:55 +0300

    Edit Home Page

---
+++ src/pages/React-movie-app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MoviesList from './MoviesList'
+
+export default function React-movie-app() {
+  return (
+    <MoviesList/>
+  )
+}
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,15 @@ import Movies from "../pages/Movies";
 import MovieDetails from "./../pages/MovieDetails";
 import ContactUs from "./../pages/ContactUs";
 import NotFound from "../pages/NotFound";
+import Favorite from "../pages/Favorite";
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Movies />}></Route>
+      <Route path="/react-movie-app" element={<Movies />}></Route>
       <Route path="/moviedetails/:id" element={<MovieDetails />}></Route>
       <Route path="/contactus" element={<ContactUs />}></Route>
+      <Route path="/favorites" element={<Favorite />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
